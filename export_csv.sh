c7f8364de9b4f2be90ac15a861364221f536e418
Krisztian Gyuris <kgyuris@cognex.com>
2016-09-16 07:02:22 +0200

    adding test data and modified exports script

--- export_csv.sh
+++ export_csv.sh
@@ -1,2 +1,2 @@
-mongoexport --db stackoverflow --collection jobs --type csv --fields jobid,title,employer,location,salary,description,tags,url,date,weeknum --out stackoverflow_jobs.csv
-mongoexport --db stackoverflow --collection jobs --type json --fields jobid,title,employer,location,salary,description,tags,url,date,weeknum --out stackoverflow_jobs.json
+mongoexport --db stackoverflow --collection jobs --type csv --fields jobid,title,employer,location,salary,description,tags,url,date,weeknum --out data/stackoverflow_jobs.csv
+mongoexport --db stackoverflow --collection jobs --type json --fields jobid,title,employer,location,salary,description,tags,url,date,weeknum --out data/stackoverflow_jobs.json
