30964b8f06b8ef6c14dc48b150db244180c6c109
Krisztian Gyuris <kgyuris@cognex.com>
2016-09-28 09:13:23 +0200

    Fixing xpaths due to site change

--- stackjobs/spiders/Stackjob.py
+++ stackjobs/spiders/Stackjob.py
@@ -13,25 +13,24 @@ class StackjobSpider(Spider):
         'https://stackoverflow.com/jobs?sort=p&pg=2',
         'https://stackoverflow.com/jobs?sort=p&pg=3'
     )
-
     def parse(self, response):
         jobs = Selector(response).xpath('//div[contains(@class, "-item") and contains(@class, "-job")]')
         results = [] 
         
         t = datetime.date.today()
         
-        for job in jobs: 
+        for job in jobs:             
             item = StackjobItem()    
-            item['date'] = t.strftime('%Y-%m-%d') 
-            item['weeknum'] = t.isocalendar()[1]                      
-            item['tags'] = job.xpath('div[contains(@class,"tags")]/p/a/text()').extract()
+            item['date'] = t.strftime('%Y-%m-%d')                           
+            item['weeknum'] = t.isocalendar()[1]                                  
+            item['tags'] = job.xpath('div[contains(@class,"tags")]/p/a/text()').extract()            
+            item['title'] = job.xpath('div[contains(@class,"-title")]/h2/a/text()').extract()[0]            
+            item['url'] = job.xpath('div[contains(@class,"-title")]/h2/a/@href').extract()[0]
             item['description'] = job.xpath('p[contains(@class,"text") and contains(@class, "description")]/text()').extract()[0]
-            item['location'] = job.xpath('ul[contains(@class, "metadata") and contains(@class, "primary")]/li[contains(@class, "location")]/text()').extract()[0].strip()
-            item['employer'] = job.xpath('ul[contains(@class, "metadata") and contains(@class, "primary")]/li[contains(@class, "employer")]/text()').extract()[0].strip()
+            item['location'] = job.xpath('div[contains(@class, "-meta-wrapper")]/ul/li[contains(@class, "location")]/text()').extract()[0].strip()
+            item['employer'] = job.xpath('div[contains(@class, "-meta-wrapper")]/ul/li[contains(@class, "employer")]/text()').extract()[0].strip()           
             item['jobid'] = job.xpath('@data-jobid').extract()[0]
-            item['title'] = job.xpath('div[contains(@class,"-title")]/h1/a/text()').extract()[0]
-            item['url'] = job.xpath('div[contains(@class,"-title")]/h1/a/@href').extract()[0]
-                        
+            
             if job.xpath('div[contains(@class,"-title")]/span[contains(@class,"salary")]/text()').extract():
                 item['salary'] = job.xpath('div[contains(@class,"-title")]/span[contains(@class,"salary")]/text()').extract()[0].strip()
 
